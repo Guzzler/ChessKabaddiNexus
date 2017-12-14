3a42c615d1b21bac286458f95c277ee80fef3c6d
Sharang Pai <sharangpai123@gmail.com>
2017-12-15 00:37:37 +0530

    connecting two peers

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var i =0;
+var playerIDArray =[];
 server.listen(8000,()=>{
   console.log("server running");
 });
 
 io.on('connection',function(socket){
   console.log("socket connected");
+  playerIDArray.push(socket.id);
+  i++;
+  socket.emit('socketID',{id: socket.id, playerStart: i})
+  if (i==2){
+    io.to(playerIDArray[0]).emit('playerConnected',{opponentID: playerIDArray[1]});
+    socket.emit('playerConnected',{opponentID: playerIDArray[0]});
+    i=0;
+    playerIDArray=[];
+  }
   socket.on('disconnect',function(){
     console.log("player disconnected");
   })
