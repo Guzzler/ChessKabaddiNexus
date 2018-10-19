b717500c091bbfe038e8c263e6083aaffe33a533
root <sharangpai123@gmail.com>
2018-10-19 14:05:02 +0000

    finalized multiplayer data collection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var fs=require('fs');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var i =0;
 var playerIDArray =[];
+var gameData=[];
 server.listen(3000,()=>{
   console.log("server running");
 });
@@ -13,19 +15,29 @@ io.on('connection',function(socket){
   i++;
   socket.emit('socketID',{id: socket.id, playerStart: i})
   if (i%2==0){
-    io.to(playerIDArray[i-2]).emit('playerConnected',{opponentID: playerIDArray[i-1]});
-    socket.emit('playerConnected',{opponentID: playerIDArray[i-2]});
+    gameID=gameData.length;
+    gameData.push([]);
+    io.to(playerIDArray[i-2]).emit('playerConnected',{opponentID: playerIDArray[i-1],gameName:gameID});
+    socket.emit('playerConnected',{opponentID: playerIDArray[i-2],gameName:gameID});
   }
   socket.on('startGame',(opponentData)=>{
     io.to(opponentData.opponentID).emit('startGame',{opponentID:socket.id});
   })
   socket.on('gameOver',(pointData)=>{
+    fs.appendFile('CollectedGameData.json', JSON.stringify(gameData[pointData.gameName])+',',function(err){
+    if (err) throw err;
+    console.log("game added successfully")
+    });
     io.to(pointData.opponentID).emit('gameOver',{points:pointData.points,opponentID:socket.id});
   })
   socket.on('defenderMove',(defenderData)=>{
+    var next_move={PieceID:defenderData.uniquePieceID, Position:defenderData.coordinates};
+    gameData[defenderData.gameID].push(next_move);
     io.to(defenderData.opponentID).emit('defenderMove',defenderData);
   })
   socket.on('attackerMove',(attackerData)=>{
+    var next_move={PieceID:attackerData.uniquePieceID, Position:attackerData.coordinates};
+    gameData[attackerData.gameID].push(next_move);
     io.to(attackerData.opponentID).emit('attackerMove',attackerData);
   })
   socket.on('disconnect',function(){
